5f80bb8e635d1e996e8286c543111563ecfb5d02
nemeljavi <nemeljavi70@gmail.com>
2024-01-15 13:07:43 +0100

    implementado boton check

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
+// configurar la aplicación
+const mensajeInicial = 'Empieza a adivinar...'
+let score = 20
+let highScore = 0
+const secretNumber = Math.trunc(Math.random() * 20) + 1
 
+// seleccionar elementos del DOM
+const messageField = document.querySelector('.message')
+const scoreField = document.querySelector('.score')
+const highScoreField = document.querySelector('.highscore')
+const secretNumberField = document.querySelector('.number')
+const checkButton = document.querySelector('.check')
+const againButton = document.querySelector('.again')
+const guessNumberField = document.querySelector('.guess')
+
+// inicializar la aplicación
+messageField.textContent = mensajeInicial
+scoreField.textContent = score
+highScoreField.textContent = highScore
+
+
+// funcionalidad de la aplicación
+
+// addEventListener es una función que recibe
+// como argumento otra función
+checkButton.addEventListener('click', () => {
+  // obtener el valor del input
+  const guessNumber = Number(guessNumberField.value)
+  if (guessNumber > secretNumber) {
+    // actualizar el mensaje
+    // actualizar el score
+    score--
+    scoreField.textContent = score
+    messageField.textContent = 'Te has pasado'
+  } else if (guessNumber < secretNumber) {
+    // actualizar el mensaje
+    // actualizar el score
+    score--
+    scoreField.textContent = score
+    messageField.textContent = 'Te has quedado corto'
+  } else {
+
+
+    // ha ganado...
+
+    
+    // cambiar fondo de pantalla
+    document.body.style.backgroundColor = 'green'
+    checkButton.disabled = true
+    // mostrar el numero secreto
+    secretNumberField.textContent = secretNumber
+    messageField.textContent = 'Has acertado'
+
+
+    // actualizar el highScore
+    if (score > highScore) {
+      highScore = score
+      highScoreField.textContent = highScore
+    }
+  }
+
+  // compararlo con el secretNumber
+})
